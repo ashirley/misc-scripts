51c5f8bd888adf8dc1c4a77dda01d01842b610d2
Andrew Shirley <andrew@als-bi.com>
2010-05-17 11:02:33 +0100

    add git status to command prompt

--- tname.sh
+++ tname.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\][\t]: \[\033[01;34m\]\w\[\033[00m\] \$ '
+#from http://gist.github.com/31631
+function parse_git_dirty {
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
+}
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]/"
+}
 
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\][\t]: \[\033[01;34m\]\w\[\033[00m\] \[\033[01;33m\]$(parse_git_branch)\[\033[00m\] \$ '
 
 if [ $# -gt 0 ]
 then
